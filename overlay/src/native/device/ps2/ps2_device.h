afd5045dbb3a6babc31796a5d4c6e69d8ff18a56
gmunhoz2019-hue <gmunhoz2019@gmail.com>
2026-09-09 03:22:27 -0300

    Add PS2 protocol-stage diagnostic counters

--- overlay/src/native/device/ps2/ps2_device.h
+++ overlay/src/native/device/ps2/ps2_device.h
@@ -12,6 +12,9 @@ extern const OutputInterface ps2_output_interface;
 uint8_t ps2_rumble_large(void);
 uint8_t ps2_rumble_small(void);
 uint32_t ps2_transaction_count(void);
+uint32_t ps2_rx_byte_count(void);
+uint32_t ps2_address_count(void);
+uint32_t ps2_poll42_count(void);
 
 #ifdef __cplusplus
 }
