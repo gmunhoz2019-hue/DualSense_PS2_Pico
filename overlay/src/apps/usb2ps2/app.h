725c0b48f75b001efcc1319e4dc32628411621e7
gmunhoz2019-hue <gmunhoz2019@gmail.com>
2026-09-09 01:52:53 -0300

    Add USB-to-PS2 app manifest

---
+++ overlay/src/apps/usb2ps2/app.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define APP_NAME "DualSense_PS2_Pico"
+#define APP_DESCRIPTION "USB Bluetooth / USB gamepad to PlayStation 2 DualShock 2"
+#define APP_AUTHOR "gmunhoz2019-hue + OpenAI"
+
+void app_init(void);
+void app_task(void);
