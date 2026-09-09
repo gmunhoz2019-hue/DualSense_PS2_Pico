56117b364e1d6fcdd2c703ef9339e65a9aaa3ca9
gmunhoz2019-hue <gmunhoz2019@gmail.com>
2026-09-09 03:11:24 -0300

    Expose PS2 bus activity diagnostic API

--- overlay/src/native/device/ps2/ps2_device.h
+++ overlay/src/native/device/ps2/ps2_device.h
@@ -11,6 +11,7 @@ extern const OutputInterface ps2_output_interface;
 
 uint8_t ps2_rumble_large(void);
 uint8_t ps2_rumble_small(void);
+uint32_t ps2_transaction_count(void);
 
 #ifdef __cplusplus
 }
